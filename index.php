08f08423f9130e0c45acf47fbe0e6d6e9532f42d
Nichlas Torgersen <post@nichiatu.com>
2014-12-13 13:59:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	/*error_reporting(E_ALL ^ E_STRICT);
 	ini_set('display_errors', true);
 	ini_set('display_startup_errors', true);*/
-
+	
 	// variables from url
 	$vars = array();
-
+	
 	// set default controller and action
 	if (!isset($_GET['controller'], $_GET['action'])) {
 		$vars['controller'] = 'home';
@@ -15,10 +15,10 @@
 		$vars['controller'] = $_GET['controller'];
 		$vars['action'] = $_GET['action'];
 	}
-
+	
 	require_once("autoload.php");
-    	require_once("vendor/autoload.php");
+	    require_once("vendor/autoload.php");
 	require_once("config.php");
-
+	
 	$app = new Application($vars['controller'], $vars['action']);
 	$app->run();
